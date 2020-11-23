f0af500d7896da5c41425e5df9494f8cd98160cd
ArataKamikaze <49700094+ArataKamikaze@users.noreply.github.com>
2020-11-23 16:12:25 -0300

    sim

--- Src/d01_utils/functions.py
+++ Src/d01_utils/functions.py
@@ -1,10 +1,10 @@
 import os as os
-from Definitions import definitions
+from d01_utils import Definitions as d
 
 def add_to_dataframe(path, date, name):
-    for x in range(1, definitions(name)):
+    for x in range(1, len(d.definitions(name))):
         for filename in os.listdir(path, date, ):
-            if filename.endswith(+".csv") and filename.beginswith(date+definitions[x]):
+            if filename.endswith(+".csv") and filename.beginswith(date+d.definitions(name)[x]):
                 print(os.path.join(path, filename))
             else:
                 continue
--- Src/d01_utils/setup.py
+++ Src/d01_utils/setup.py
@@ -1,18 +1,18 @@
 import os as os
 from pathlib import Path
-class setup:
 
-    def setup():
 
-        if not Path('../../Data').is_dir():
-            os.mkdir('../../Data')
-        if not Path('../../Data/00_Zip').is_dir():
-            os.mkdir('../../Data/00_Zip')
-        if not Path('../../Data/01_raw').is_dir():
-            os.mkdir('../../Data/01_raw')
-        if not Path('../../Data/02_Processed').is_dir():
-            os.mkdir('../../Data/02_Processed')
+def setup():
 
-        if Path('../../Data').is_dir() and Path('../../Data/00_Zip').is_dir() and Path('../../Data/01_raw').is_dir() and Path('../../Data/02_Processed').is_dir():
-            print("Setup Successfull")
-    setup()
+    if not Path('../../Data').is_dir():
+        os.mkdir('../../Data')
+    if not Path('../../Data/00_Zip').is_dir():
+        os.mkdir('../../Data/00_Zip')
+    if not Path('../../Data/01_raw').is_dir():
+        os.mkdir('../../Data/01_raw')
+    if not Path('../../Data/02_Processed').is_dir():
+        os.mkdir('../../Data/02_Processed')
+
+    if Path('../../Data').is_dir() and Path('../../Data/00_Zip').is_dir() and Path('../../Data/01_raw').is_dir() and Path('../../Data/02_Processed').is_dir():
+        print("Setup Successfull")
+setup()
